1cbbb6a6f00fcb78306aea06379057e8da83b861
Juan Penaloza <penalozahernandezjuandavid03@gmail.com>
2022-11-28 12:33:02 -0500

    Configurando rutas 5

--- JS/clase.js
+++ JS/clase.js
@@ -30,25 +30,25 @@ class Restaurante {
 }
 
 const restaurante1 = new Restaurante(`Restaurante El Argentino`, `Distinguido restaurante con culinaria especializada 
-en gustosos platos con ingrediente principal, la carne, la especialidad de la casa.`, `../Images/R_Argentino.png`, 
+en gustosos platos con ingrediente principal, la carne, la especialidad de la casa.`, `Images/R_Argentino.png`, 
 `En este establecimiento nos sentimos orgullosos de hacerle degustar a nuestros comensales la jugosa experiencia de 
 consumir la magnifica carne argentina que ha sido simpre tan distinguida, donde se sabe que cada gramo es una sensación 
 inigualable.`);
 
 const restaurante2 = new Restaurante(`Restaurante Elegante`, `Te mereces lo mejor de esta vida y tu apetito también.`, 
-`../Images/R_Elegante.PNG`, `La característica "cocina creativa" deja mucho qué pensar. Pues entrar a El Cielo es 
+`Images/R_Elegante.PNG`, `La característica "cocina creativa" deja mucho qué pensar. Pues entrar a El Cielo es 
 encontrarse literalmente con eso: platos nuevos, originales y creaciones del chef, quién crea sopas, carnes, pescados y 
 postres con diferentes productos, como por ejemplo su postre de limón. El diseño de El Cielo te hará sentir tranquilo, 
 ya que se encuentra decorado con árboles que le dan una sensación natural.`);
 
 const restaurante3 = new Restaurante(`Restaurante Marino`, `El sabor inigualable de las aguas marinas.`, 
-`../Images/R_Marino.png`, `Con una larga trayectoria gastronómica internacional. Frutos del Mar Patrimonio 
+`Images/R_Marino.png`, `Con una larga trayectoria gastronómica internacional. Frutos del Mar Patrimonio 
 gastronómico de la Ciudad es un lugar acogedor con un equipo de colaboradores profesionales idóneos en la empresa.
 Este restaurante especializado en comida de mar, sobresale por sus exquisitos pescados en varias preparaciones, 
 sus mariscos y sus vinos.`);
 
 const restaurante4 = new Restaurante(`Restaurante Mexicano`, `Conoceras la delicia del picante.`,
-`../Images/R_Mexicano.png`, `La plaza mayor de la ciudad de México es la que le da el nombre a este restaurante 
+`Images/R_Mexicano.png`, `La plaza mayor de la ciudad de México es la que le da el nombre a este restaurante 
 que ofrece a sus visitantes la autentica tradición gastronómica de esta nación. Tampico, Puebla y Veracruz son 
 algunos de los estados que inspiran las recetas (preparadas con ingredientes importados) y la decoración de sus 
 espacios ambientados con zócalos, máscaras y cerámicas que evocan lo mejor del país de los mariachis y corridos.`);
